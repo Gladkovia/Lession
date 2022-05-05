4b742dc360c1d9fe3e4b60fa06d1afa1da371ef6
Gladkovian <gladkovian@gmail.com>
2022-05-05 17:16:12 +0300

    color

--- Lession1.py
+++ Lession1.py
@@ -3,15 +3,15 @@ from colorama import Fore, Back, Style
 
 colorama.init()
 
-
+print(Fore.BLACK)
 print(Back.GREEN)
 what = input( "Что делаем (+, -):" )
 print(Back.RED)
 a = float(input("Введи первое чило: ") )
-print(Back.GREEN)
+print(Back.CYAN)
 b = float(input("Ввиди второе число: ") )
 
-print(Back.RED)
+print(Back.YELLOW)
 if what == "+":
     c = a + b
     print("Результат " + str(c))
