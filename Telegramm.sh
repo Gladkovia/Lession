d96383bbcfd301f89e837814312f17280660e4fe
Gladkovian <gladkovian@gmail.com>
2022-05-05 10:53:45 +0300

    telega1

--- Telegramm.sh
+++ Telegramm.sh
@@ -1,55 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import telebot
-
 from telebot import apihelper
-
 import sys
-
 import datetime
-
 import time
-
-
-
 def log(message):
-
     file_name = '/root/log/log.txt'
-
     file3 = open(file_name,'a')
-
     file3.write(str(datetime.datetime.now())+' '+str(message)+'\n')
-
     file3.close()
-
-
-
 for er in range(0,8):
-
-
-
-
-
        try:
-
-
-
-
-
                 token = "you key"
-
                 bot = telebot.TeleBot(token)
-
-
-
-
-
 #                apihelper.proxy = {'https': 'socks5://207.97.174.134:1080'}
-
-
-
                 bot.send_message(-317729700, 'Критическое сообщение:\n'+ sys.argv[2] +'\n'+
-
                                 '__________________________\n'+
 
                                 'Время регистрации:\n'+sys.argv[1] +'\n'+
@@ -63,16 +29,9 @@ for er in range(0,8):
                                 'IP Адрес\n'+sys.argv[6]+'\n'+
 
                                 '__________________________\n')
-
-
                 break
-
        except Exception as e:
-
                msg = ' '.join(sys.argv[1:])
-
                log(msg)
-
                print (str(e))
-
                time.sleep(3)
