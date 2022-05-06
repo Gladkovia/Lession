2fab1f781ff8c900621de03bfe5d311dd6be9efb
Gladkovian <gladkovian@gmail.com>
2022-05-06 09:55:05 +0300

    ver

--- Lession2.py
+++ Lession2.py
@@ -1,29 +1,19 @@
 import pyowm
-#from pyowm import timestamps
 from pyowm import OWM
-#from pyowm.utils import config
-#from pyowm.utils import timestamps
 
 place = input("В каком городе: ")
 owm = OWM('b6afb6109ec39d64c7829045fbb8fd74', language = "ru")
-#mgr = owm.weather_manager()
+
 observation = owm.weather_at_place(place)
 w = observation.get_weather()
 status = w.get_detailed_status()
 temp = w.get_temperature('celsius')["temp"]
 print("В вашем городе " + place + " сейчас " + status )
-print("Температура в " + place + "ровна " + str(temp))
+print("Температура в " + place + " ровна " + str(temp))
 
-#place = input("В каком городе: ")
-# Search for current weather in London (Great Britain) and get details
-#observation = owm.weather_at_place(place)
-#w = observation.get_weather()
-#print(w)
-
-
-#owm = pyowm.OWM('0b9240d2be6d5cbf77a3f760c0e2c61c')
-
-#place = input("В каком городе: ")
-#observation = owm.weather_at_place(place)
-#w = observation.weather
-#print(w)
+if temp < 5:
+    print("Сейчас холодно, нужна шапка! " + str(temp))
+elif temp < 20:
+    print("Сейчас тепло! " + str(temp) )
+else:
+    print("Сейчас " + str(temp) + " на твое усмотрение")
